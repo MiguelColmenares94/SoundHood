b09b8dda338ad87a2ffaf0edfee44a21d7d8df53
mavix21 <marcevizcarra2109@hotmail.com>
2023-06-29 22:31:08 -0500

    Feat: Swipe functionality

---
+++ soundhood/static/scripts/index.js
@@ -0,0 +1,19 @@
+let activeIndex = 0;
+const groups = document.getElementsByClassName("user-card");
+console.log('Hi from script')
+
+const handleLoveClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  // Active group becomes after
+  
+  currentGroup.dataset.status = "after";
+
+  // Next group becomes active
+  
+  nextGroup.dataset.status = "active";
+
+  activeIndex = nextIndex;
+}
